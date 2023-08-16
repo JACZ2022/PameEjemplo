6319ed0b1ea9b361a2e7df56013ab37b14d18c44
JACZ2022 <antonio.cz@apizaco.tecnm.mx>
2023-08-16 11:22:03 -0600

    biometricos

--- PAME/vigilancia/models.py
+++ PAME/vigilancia/models.py
@@ -1,85 +1,116 @@
+from tabnanny import verbose
 from django.db import models
-from Catalogos.models import Estacion, Responsable
+from catalogos.models import Estacion
 
-
-
-# Create your models here.
-class TipoDisposicion(models.Model):
-    numero = models.IntegerField()
-    descripcion = models.CharField(max_length=50)
+class Nacionalidad(models.Model):
+    nombre = models.CharField(max_length=200,verbose_name='Nacionalidad')
+    Abreviatura = models.CharField(max_length=200,verbose_name='Abreviatura')
     
+    class Meta:
+        verbose_name_plural = "Nacionalidades"
+    
+    def __str__(self):
+        return self.nombre
 
-
-
-class PuestaDisposicion(models.Model):
+class PuestaDisposicionINM(models.Model):
     numeroOficio = models.CharField(max_length=50)
     fechaOficio = models.DateField()
     nombreAutoridadSigna = models.CharField(max_length=100)
     cargoAutoridadSigna = models.CharField(max_length=100)
-    oficioPuesta = models.CharField(max_length=100)
-    oficioComision = models.CharField(max_length=100)
-    deLaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
-    deLadisposicion = models.ForeignKey(TipoDisposicion, on_delete=models.CASCADE)
-   
-
-class Extranjero(models.Model):
-    fechaRegistro = models.DateField()
-    horaRegistro = models.DateTimeField()
-    numeroExtranjero = models.IntegerField()
-    estacionMigratoria = models.CharField(max_length=50)
-    nombreExtranjero = models.CharField(max_length= 50)
-    apellidoPaternoExtranjero = models.CharField(max_length=50)
-    apellidoMaternoExtranjero = models.CharField(max_length=50)
-    firmaExtranjero = models.CharField(max_length=100)
-    huellaExtranjero = models.CharField(max_length=100)
-    nacionalidad = models.CharField(max_length=50)
-    genero = models.CharField(max_length= 10)
-    fechaNacimiento = models.DateField()
-    documentoIdentidad = models.CharField(max_length=100)
-    fotografiaExtranjero = models.CharField(max_length=100)
-    viajaSolo = models.BooleanField()
-    voluntadPropia = models.BooleanField()
-    tipoEstancia = models.CharField(max_length=50)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
-    
-class Proceso(models.Model):
-    numperoUnicoProceso = models.IntegerField()
-    delResponsable = models.ForeignKey(Responsable, on_delete= models.CASCADE)
-    delExtranjero = models.ForeignKey(Extranjero, on_delete= models.CASCADE)
-    
-class ComplementoINM(models.Model):
+    oficioPuesta = models.FileField(upload_to='files/',  null=True, blank=True)
+    oficioComision = models.FileField(upload_to='files/',  null=True, blank=True)
     puntoRevision = models.CharField(max_length=100)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
+    deLaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Puestas a Disposición INM"
+
+    def __str__(self):
+        return self.numeroOficio
     
- 
+    @property
+    def extranjeros(self):
+        return self.extranjeros.all()
     
-     
-class ComplementonAC(models.Model):
+
+class PuestaDisposicionAC(models.Model):   
+    numeroOficio = models.CharField(max_length=50)
+    fechaOficio = models.DateField()
+    nombreAutoridadSigna = models.CharField(max_length=100)
+    cargoAutoridadSigna = models.CharField(max_length=100)
+    oficioPuesta = models.FileField(upload_to='files/',  null=True, blank=True)
+    oficioComision = models.FileField(upload_to='files/',  null=True, blank=True)
+    puntoRevision = models.CharField(max_length=100)
     dependencia = models.CharField(max_length=100)
-    numeroCarpeta = models.CharField(max_length=30)
+    numeroCarpeta = models.IntegerField()
     entidadFederativa = models.CharField(max_length=100)
-    certificadoMedico = models.CharField(max_length=100)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
-    
-class ComplementoTraslado(models.Model):
-    estacionOrigen = models.CharField(max_length=100)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
+    certificadoMedico = models.FileField(upload_to='files/',  null=True, blank=True)
+    deLaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE, verbose_name="Estación de origen", null=True, blank=True)
 
-class ComplementoPorTrasladar(models.Model):
-    estacionDestino = models.CharField(max_length=100)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Puestas a Disposicion AC"
+    
+    def __str__(self):
+        return str(self.numeroOficio) 
     
     
-class complementoAlojamiento(models.Model):
-    estacionOrigen = models.CharField(max_length=100)
-    deLaPuesta = models.ForeignKey(PuestaDisposicion, on_delete= models.CASCADE)
 
+OPCION_GENERO_CHOICES=[
+    [0,'HOMBRE'],
+    [1,'MUJER'],
+]
+class Extranjero(models.Model):
+    fechaRegistro = models.DateField(auto_now_add=True)
+    horaRegistro = models.DateTimeField(auto_now_add=True)
+    numeroExtranjero = models.IntegerField(blank=True, null=True)
+    estacionMigratoria = models.CharField(max_length=50,blank=True)
+    nombreExtranjero = models.CharField(max_length= 50, blank=True)
+    apellidoPaternoExtranjero = models.CharField(max_length=50, blank=True)
+    apellidoMaternoExtranjero = models.CharField(max_length=50, blank=True)
+    nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE, verbose_name="Nacionalidad")
+    genero = models.IntegerField(choices=OPCION_GENERO_CHOICES)
+    fechaNacimiento = models.DateField()
+    documentoIdentidad = models.FileField(upload_to='files/',  null=True,blank=True)
+    viajaSolo = models.BooleanField()
+    tipoEstancia = models.CharField(max_length=50, blank=True)
+    deLaPuestaIMN = models.ForeignKey(PuestaDisposicionINM, on_delete= models.CASCADE,blank=True, null=True, related_name='extranjeros',verbose_name='Puesta')
+    deLaPuestaAC = models.ForeignKey(PuestaDisposicionAC, on_delete= models.CASCADE,blank=True, null=True, related_name='extranjeros', verbose_name='Puesta')
+
+    class Meta:
+        verbose_name_plural = "Extranjeros" 
+    # def __str__(self):
+    #     return self.numeroExtranjero, self.estacionMigratoria, self.nombreExtranjero
+    
+    
+OPCION_RELACION_CHOICES=[
+    [0,'ESPOSO(A)'],
+    [1,'HIJO(A)'],
+    [2,'MADRE'],
+    [3,'PADRE'],
+    [4,'OTRO'],
+]
 class Acompanante(models.Model):
     delExtranjero = models.IntegerField()
-    delAcompanante = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
-    relacion = models.CharField(max_length=30)
-    
-    
+    delAcompanante = models.ForeignKey(Extranjero, on_delete=models.CASCADE, blank=True, null=True)
+    relacion = models.IntegerField(choices=OPCION_RELACION_CHOICES)
 
-    
-     
+    class Meta:
+        verbose_name_plural = "Acompañantes"
+
+class Biometrico(models.Model):
+    Extranjero = models.OneToOneField(
+        Extranjero,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    fotografiaExtranjero = models.FileField(upload_to='files/',  null=True,blank=True)
+    fechaHoraFotografia = models.DateTimeField()
+    huellaExtranjero = models.FileField(upload_to='files/',  null=True,blank=True)
+    fechaHorauHuella = models.DateTimeField()
+    firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
+    fechaHoraFirma = models.DateTimeField()
+
+
+    class Meta:
+        verbose_name_plural = "Biometricos"
+        
