e4442cbee44dd7f69d84963715017864df943500
José Antonio Cruz Zamora <97242945+JACZ2022@users.noreply.github.com>
2023-09-07 12:46:21 -0600

    Traslados modificación 7-09-2023

--- models.py
+++ models.py
@@ -4,17 +4,26 @@ from vigilancia.models import Extranjero, Proceso
 
 # Create your models here.
 
-class traslado(models.Model):
-    numeroOficio = models.CharField(max_length=50)
-    fechaOficio = models.DateTimeField()
-    nombreAutoridadSigna = models.CharField(max_length=100)
-    cargoAutoridadSigna = models.CharField(max_length=100)
-    fechaTraslado = models.DateTimeField()
-    fechaArrivo = models.DateTimeField()
+
+class Traslado(models.Model):
+    numeroUnicoProceso = models.CharField(max_length=50)
     estacionOrigen = models.CharField(max_length=50)
     estacionDestino = models.ForeignKey(Estacion, on_delete=models.CASCADE, null=True, blank=True)
-    estatus = models.CharField(max_length=50)
-    numeroUnicoProceso = models.ForeignKey(Proceso,  on_delete=models.CASCADE)
+    fechaSolicitud = models.DateTimeField()
+    fechaAceptacion = models.DateTimeField()
+    fechaTraslado = models.DateTimeField()
+    fechaArrivo = models.DateTimeField()
+    nombreAutoridadEnvia = models.CharField(max_length=100)
+    nombreAutoridadRecibe = models.CharField(max_length=100)
+    responsableEnvia = models.CharField(max_length=100)
+    responsableRecibe = models.CharField(max_length=100)
+    enTraslado = models.BooleanField()
+    status = models.CharField(max_length=50)
+    
+class ExtranjeroTraslado(models.Model):
+    statusTraslado = models.CharField(max_length=50)
+    delTraslado = models.ForeignKey(Traslado, on_delete=models.CASCADE, null=True, blank=True)
+    delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Alojamiento(models.Model):
