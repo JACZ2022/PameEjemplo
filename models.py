d4a0873516b677ce13765c1de027c1b97b1ddfbd
José Antonio Cruz Zamora <97242945+JACZ2022@users.noreply.github.com>
2023-09-08 13:19:45 -0600

    Traslado_choices

--- models.py
+++ models.py
@@ -4,10 +4,20 @@ from vigilancia.models import Extranjero, Proceso
 
 # Create your models here.
 
+OPCION_STATUS_CHOICES=[
+    [0,'SOLICITUD'],
+    [1,'ACEPTADO'],
+    [2,'RECHAZADO'],
+]
+
+OPCION_STATUS_TRASLADO_CHOICES=[
+    [0,'ACPTADO'],
+    [1,'RECHAZADO'],
+]
 
 class Traslado(models.Model):
     numeroUnicoProceso = models.CharField(max_length=50)
-    estacionOrigen = models.CharField(max_length=50)
+    estacionOrigen = models.ForeignKey(Estacion, on_delete=models.CASCADE, null=True, blank=True)
     estacionDestino = models.ForeignKey(Estacion, on_delete=models.CASCADE, null=True, blank=True)
     fechaSolicitud = models.DateTimeField()
     fechaAceptacion = models.DateTimeField()
@@ -18,10 +28,10 @@ class Traslado(models.Model):
     responsableEnvia = models.CharField(max_length=100)
     responsableRecibe = models.CharField(max_length=100)
     enTraslado = models.BooleanField()
-    status = models.CharField(max_length=50)
+    status = models.IntegerField(choices=OPCION_STATUS_CHOICES)
     
 class ExtranjeroTraslado(models.Model):
-    statusTraslado = models.CharField(max_length=50)
+    statusTraslado = models.IntegerField(choices=OPCION_STATUS_TRASLADO_CHOICES)
     delTraslado = models.ForeignKey(Traslado, on_delete=models.CASCADE, null=True, blank=True)
     delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE, null=True, blank=True)
 
