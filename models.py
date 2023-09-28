7467001ba79243d18c6d62de9b9ad1d757ff1edf
José Antonio Cruz Zamora <97242945+JACZ2022@users.noreply.github.com>
2023-09-27 18:03:39 -0600

    Certificado Médico

--- models.py
+++ models.py
@@ -1,45 +1,74 @@
+
 from django.db import models
-from vigilancia.models import Extranjero
-from catalogos.models import Estacion, Responsable
+from catalogos.models import Estacion
+from vigilancia.models import Extranjero, Proceso
+from consultaMedica.models import PerfilMedico
 
 # Create your models here.
-class TipoAcuerdo(models.Model):
-    tipo = models.CharField()
 
-class Acuerdo(models.Model):
-    delAcuerdo = models.ForeignKey(TipoAcuerdo, on_delete=models.CASCADE)
-    fechaAcuerdo = models.DateField()
-    numeroExpediente = models.CharField(max_length=50)
-    horaAcuerdo = models.TimeField()
-    deLaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE, null=True, blank=True)
-    delResponsable = models.ForeignKey(Responsable, on_delete=models.CASCADE)
-    delExtanjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
-    nombreTestigoUno = models.CharField(max_length=50)
-    apellidoPaternoTestigoUno = models.CharField(max_length=50)
-    apellidoMaternoTestigoUno = models.CharField(max_length=50)
-    firmaTestigoUno = models.BinaryField()
-    huellaTestigoUno = models.BinaryField()
-    nombreTestigoDos = models.CharField(max_length=50)
-    apellidoPaternoTestigoDos = models.CharField(max_length=50)
-    apellidoMaternoTestigoDos = models.CharField(max_length=50)
-    firmaTestigoDos = models.BinaryField()
-    huellaTestigoDos = models.BinaryField()
-    delExtanjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
-    nombreTraductor = models.CharField(max_length=50)
-    apellidoPaternoTraductor = models.CharField(max_length=50)
-    apellidoMaternoTraductor = models.CharField(max_length=50)
-    razonContinuidad_Continuacion = models.TextField(null=True, blank = True)
-    fechaSuspensión_Continuacion = models.DateField(null=True, blank=True)
-    autoridadEmisora_Acumulacion = models.TextField(null=True, blank = True)
-    numeroExpedienteAnterior_Acumulacion =models.CharField(max_length=50, null=True, blank =True)
-    antecedentes_conclusion = models.TextField(null=True, blank = True)
-    delegatorio_inicio = models.TextField(null=True, blank = True)
-    descripcion_recepcion = models.TextField(null=True, blank = True)
-    fundamento_separacion = models.TextField(null=True, blank = True)
-    razon_suspension = models.TextField(null=True, blank = True)
-    declaracionExtanjero_comparecencia = models.TextField(null=True, blank = True)
-    estacionDestino_traslado = models.CharField(max_length=50, null=True, blank = True)
-    motivo_traslado = models.TextField(null=True, blank = True)
-    documentoAcuerdo= models.FileField(upload_to='files/', null=True, blank=True)
-
-
+OPCION_TIPO_CHOICES= [
+    [0,'INGRESO'],
+    [1,'EGRESO'],
+]
+class CertificadoMedico(models.Model):
+    delProceso = models.ForeignKey(Proceso, on_delete= models.CASCADE)
+    delaEstacion = models.ForeignKey(Estacion, on_delete= models.CASCADE)
+    delMedico = models.ForeignKey(PerfilMedico, on_delete=models.CASCADE)
+    fechaCertificado = models.DateField()
+    horaCertificado = models.CharField(max_length=50)
+    tipoCertificado = models.CharField(verbose_name='Tipo Certificado', max_length=25, choices=OPCION_TIPO_CHOICES, default='INGRESO')
+# Exploracion física
+    temperatura = models.DecimalField(max_digits=10, decimal_places=2)
+    frecuenciaRespiratoria = models.IntegerField()
+    presionArterialSistolica = models.IntegerField()
+    presionArterialDiastolica =models.IntegerField()
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
+    estatura = models.DecimalField(max_digits=10, decimal_places=2)
+    oxigenacionSaturacion = models.DecimalField(max_digits=10, decimal_places=2)
+    oxigenacionFrecuencia = models.DecimalField(max_digits=10, decimal_places=2)
+    indiceMasaCorporal = models.DecimalField(max_digits=10, decimal_places=2)
+# Antecedentes
+    hepatitis = models.BooleanField()
+    tubercolisis = models.BooleanField()
+    paludismo = models.BooleanField()
+    dengue = models.BooleanField()
+    colera = models.BooleanField()
+    hipertension = models.BooleanField()
+    cardiopatias = models.BooleanField()
+    vih = models.BooleanField()
+    otrosAntecedentes = models.TextField()
+    antecedentesQuirurgicos = models.TextField()
+    padecimientosCronicos= models.TextField()
+    alergias = models.TextField()
+    delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
+#   Padecimientos actuales
+    diarrea = models.BooleanField()
+    infeccionesRespiratorias =models.BooleanField()
+    fiebre=models.BooleanField()
+    hemorragias = models.BooleanField()
+    nauseasVomito= models.BooleanField()
+    tos = models.BooleanField()
+    dolores=models.BooleanField()
+    lesionesPiel = models.BooleanField()
+    mareosVertigo=models.BooleanField()
+    tabaquismo=models.BooleanField()
+    bebidasAlcoholicas = models.BooleanField()
+    toxicomanias = models.TextField()
+    tipoDieta = models.CharField(max_length=50)
+    sintomasCovid=models.TextField()
+    embarazo = models.BooleanField()
+    tiempoEmbarazo=models.DecimalField(max_digits=10, decimal_places=2)
+    conclusiondiagnostica= models.TextField()
+    observaciones =models.TextField()
+    tratamiento = models.TextField()
+    
+class certificadoExterno(models.Model):
+    delProceso = models.ForeignKey(Proceso, on_delete= models.CASCADE)
+    delaEstacion = models.ForeignKey(Estacion, on_delete= models.CASCADE)
+    delMedico = models.ForeignKey(PerfilMedico, on_delete=models.CASCADE)
+    fechaCertificado = models.DateField()
+    horaCertificado = models.CharField(max_length=50)
+    diagnostico = models.TextField()
+    unidadMedica = models.TextField()
+    documentoAtencionExterna = models.FileField(verbose_name="Certificado Externo:",upload_to='files/', null=True, blank=True)
+    
