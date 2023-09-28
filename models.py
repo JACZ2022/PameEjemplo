4c23ab00cc695c3833149fdcc0405687f2314a93
José Antonio Cruz Zamora <97242945+JACZ2022@users.noreply.github.com>
2023-09-27 18:04:38 -0600

    Consulta Médica

--- models.py
+++ models.py
@@ -1,22 +1,27 @@
-
 from django.db import models
-from catalogos.models import Estacion
-from vigilancia.models import Extranjero, Proceso
-from consultaMedica.models import PerfilMedico
 
 # Create your models here.
+from django.db import models
+from vigilancia.models import Extranjero, Proceso
+from catalogos.models import Estacion
 
-OPCION_TIPO_CHOICES= [
-    [0,'INGRESO'],
-    [1,'EGRESO'],
-]
-class CertificadoMedico(models.Model):
+
+
+    
+
+class PerfilMedico(models.Model):
+    nombreMedico = models.CharField(max_length=50)
+    apellidoPaternoMedico = models.CharField(max_length=50)
+    apellidoMaternoMedico = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=50)
+
+
+class Consulta(models.Model):
     delProceso = models.ForeignKey(Proceso, on_delete= models.CASCADE)
     delaEstacion = models.ForeignKey(Estacion, on_delete= models.CASCADE)
     delMedico = models.ForeignKey(PerfilMedico, on_delete=models.CASCADE)
-    fechaCertificado = models.DateField()
-    horaCertificado = models.CharField(max_length=50)
-    tipoCertificado = models.CharField(verbose_name='Tipo Certificado', max_length=25, choices=OPCION_TIPO_CHOICES, default='INGRESO')
+    fechaConsulta = models.DateField()
+    horaConsulta = models.DateTimeField()
 # Exploracion física
     temperatura = models.DecimalField(max_digits=10, decimal_places=2)
     frecuenciaRespiratoria = models.IntegerField()
@@ -27,48 +32,32 @@ class CertificadoMedico(models.Model):
     oxigenacionSaturacion = models.DecimalField(max_digits=10, decimal_places=2)
     oxigenacionFrecuencia = models.DecimalField(max_digits=10, decimal_places=2)
     indiceMasaCorporal = models.DecimalField(max_digits=10, decimal_places=2)
-# Antecedentes
-    hepatitis = models.BooleanField()
-    tubercolisis = models.BooleanField()
-    paludismo = models.BooleanField()
-    dengue = models.BooleanField()
-    colera = models.BooleanField()
-    hipertension = models.BooleanField()
-    cardiopatias = models.BooleanField()
-    vih = models.BooleanField()
-    otrosAntecedentes = models.TextField()
-    antecedentesQuirurgicos = models.TextField()
-    padecimientosCronicos= models.TextField()
-    alergias = models.TextField()
-    delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
-#   Padecimientos actuales
-    diarrea = models.BooleanField()
-    infeccionesRespiratorias =models.BooleanField()
-    fiebre=models.BooleanField()
-    hemorragias = models.BooleanField()
-    nauseasVomito= models.BooleanField()
-    tos = models.BooleanField()
-    dolores=models.BooleanField()
-    lesionesPiel = models.BooleanField()
-    mareosVertigo=models.BooleanField()
-    tabaquismo=models.BooleanField()
-    bebidasAlcoholicas = models.BooleanField()
-    toxicomanias = models.TextField()
-    tipoDieta = models.CharField(max_length=50)
-    sintomasCovid=models.TextField()
-    embarazo = models.BooleanField()
-    tiempoEmbarazo=models.DecimalField(max_digits=10, decimal_places=2)
-    conclusiondiagnostica= models.TextField()
-    observaciones =models.TextField()
-    tratamiento = models.TextField()
     
-class certificadoExterno(models.Model):
+    
+class ConsultaExterna(models.Model):
     delProceso = models.ForeignKey(Proceso, on_delete= models.CASCADE)
     delaEstacion = models.ForeignKey(Estacion, on_delete= models.CASCADE)
     delMedico = models.ForeignKey(PerfilMedico, on_delete=models.CASCADE)
-    fechaCertificado = models.DateField()
-    horaCertificado = models.CharField(max_length=50)
+    fechaCconsulta = models.DateField()
+    horaConsulta = models.CharField(max_length=50)
     diagnostico = models.TextField()
     unidadMedica = models.TextField()
-    documentoAtencionExterna = models.FileField(verbose_name="Certificado Externo:",upload_to='files/', null=True, blank=True)
+    documentoAtencionExterna = models.FileField(verbose_name="Consulta externa",upload_to='files/', null=True, blank=True)
     
+class RecetaMedica(models.Model):
+    diagnostico = models.TextField()
+    delaConsulta = models.ForeignKey(Consulta, on_delete=models.CASCADE, null = True, blank= True)
+    delaConsultaExterna = models.ForeignKey(ConsultaExterna, on_delete=models.CASCADE, null = True, blank= True)
+ 
+class detalleTratmiento(models.Model):
+    dosis = models.IntegerField()
+    unidad = models.CharField(max_length=50)
+    nombreMedicamento = models.CharField(max_length=50)
+    descipcion = models.CharField(max_length=100)
+    delaReceta = models.ForeignKey(RecetaMedica, on_delete=models.CASCADE)
+
+
+    
+
+    
+# Create your models here.
