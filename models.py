52481c69dca8d2bb4973b320c2ec3c28638f1541
José Antonio Cruz Zamora <97242945+JACZ2022@users.noreply.github.com>
2023-11-06 17:56:53 -0600

    Notificaciones

--- models.py
+++ models.py
@@ -1,116 +1,63 @@
 from django.db import models
-
-
+from vigilancia.models import Proceso, Estacion, Extranjero
+from comparecencia.models import Comparecencia
+from catalogos.models import AutoridadActuante, Consulado, Fiscalia
 # Create your models here.
-class Tipos(models.Model):
-    tipo = models.CharField(max_length=50, null=False)
-
-    def __str__(self) -> str:
-        return self.tipo
-    
-    class Meta:
-        verbose_name_plural = "Tipos"
-    
-class Estatus(models.Model):
-    tipoEstatus = models.CharField(max_length=20, null=False)
-
-    def __str__(self) -> str:
-        return self.tipoEstatus
-    
-    class Meta:
-        verbose_name_plural = "Estatus"
-    
-class Estado(models.Model):
-    estado = models.CharField(max_length=50, null=False)
-
-    def __str__(self) -> str:
-        return self.estado
-    
-class Fiscalia(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    direccion = models.CharField(max_length=100, null=False)
-    correoElectronico = models.CharField(max_length=100, null=False)
-    delEstado = models.ForeignKey(Estado, on_delete=models.CASCADE)
-
-class Consulado(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    direccion = models.CharField(max_length=100, null=False)
-    correoElectronico = models.CharField(max_length=100, null=False)
-
-    def __str__(self) -> str:
-        return self.nombre
-    
-class Responsable(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    apellidoPat = models.CharField(max_length=50, null=False)
-    apellidoMat = models.CharField(max_length=50, null=False)
-    cargo = models.CharField(max_length=50, null=False)
-    email = models.EmailField(max_length=254, null=False)
-    telefono = models.CharField(max_length=10, null=False)
-
-    def __str__(self) -> str:
-        return self.nombre
-    
-class AutoridadActuante(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    apellidoPat = models.CharField(max_length=50, null=False)
-    apellidoMat = models.CharField(max_length=50, null=False)
-    cargo = models.CharField(max_length=50, null=False)
-    email = models.EmailField(max_length=254, null=False)
-    telefono = models.CharField(max_length=10, null=False)
-
-    def __str__(self) -> str:
-        return self.nombre
-    
 
 
-class OficinaRepresentacion(models.Model):
-    identificador = models.CharField(max_length=10, null=False)
-    nombre = models.CharField(max_length=50, null=False)
-    delEstado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+class Notificacion(models.Model):
+    fechaAceptacion = models.DateField(verbose_name='Fecha de Aceptación', auto_now_add=True)
+    no_proceso = models.ForeignKey(Proceso, on_delete=models.CASCADE, verbose_name='Número de Proceso Asociado', related_name='notificaciones_juridico')
+    estacion = models.ForeignKey(Estacion, on_delete=models.CASCADE, verbose_name='Estación de Notificación')
+    
+    # Campos adicionales que podrías necesitar en el futuro...
+    descripcion = models.TextField(verbose_name='Descripción', blank=True, null=True)
+    estatus_notificacion = models.CharField(max_length=50, verbose_name='Estatus de Notificación', blank=True, null=True)
 
-OPCION_CLASE_CHOICES=[
-    ('ESTACION','estacion'),
-    ('ESTANCIA','estancia'),
-]
+    def __str__(self):
+        return f"Notificación de {self.no_proceso.extranjero.nombreExtranjero} en {self.estacion.nombre}"
 
-class Estacion(models.Model):
-    identificador = models.CharField(max_length=10, null=False)
-    nombre = models.CharField(max_length=50, null=False)
-    calle = models.CharField(max_length=50, null=False)
-    noext = models.CharField(max_length=5)
-    noint = models.CharField(max_length=5, default='sn')
-    colonia = models.CharField(max_length=50, null=False)
-    cp = models.IntegerField(null=False)
-    poblacion = models.CharField(max_length=50, null= False)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=254, null=False)
-    responsable = models.ForeignKey(Responsable, on_delete=models.CASCADE)
-    tipo = models.ForeignKey(Tipos, on_delete=models.CASCADE)
-    clase = models.CharField(verbose_name='Clase estacion', max_length= 20, choices = OPCION_CLASE_CHOICES, default='Estacion')
-    estatus = models.ForeignKey(Estatus, on_delete=models.CASCADE)
-    capacidad = models.IntegerField( null=False)
-    delaOficina = models.ForeignKey(OficinaRepresentacion, on_delete=models.CASCADE)
-    
-    def __str__(self) -> str:
-        return self.nombre
-    
-    class Meta:
-        verbose_name_plural = "Estaciones"
-class Salida(models.Model):
-    tipoSalida = models.CharField(max_length=50)
-    
-    def __str__(self) -> str:
-        return self.tipoSalida
-    
-class Estancia(models.Model):
-    tipoEstancia = models.CharField(max_length=50)
-    
-    def __str__(self) -> str:
-        return self.tipoEstancia
-    
-class Relacion(models.Model):
-    tipoRelacion = models.CharField(max_length=50)
-    
-    def __str__(self) -> str:
-        return self.tipoRelacion
+ACCION= (
+    ('Deportacion', 'DEPORTACION'),
+    ('Retorno_Asistido', 'RETORNO ASISTIDO'),
+)
+
+class NotificcionConsular(models.Model):
+    delaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
+    fechaNotificacion= models.DateField()
+    horaNotificacion = models.DateTimeField()
+    numeroOficio = models.CharField(max_length=50)
+    delConsulado = models.ForeignKey(Consulado, on_delete=models.CASCADE)
+    delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
+    accion = models.CharField(max_length=50, choices= ACCION)
+    delaAutoridad = models.ForeignKey(AutoridadActuante, on_delete=models.CASCADE)
+
+class NotificacionCOMAR(models.Model):
+    delaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
+    fechaNotificacion= models.DateField()
+    numeroOficio = models.CharField(max_length=50)
+    delExtranjero= models.ForeignKey(Extranjero, on_delete=models.CASCADE)
+    #Datos COMAR
+    notificacionComar= models.TextField()
+    delProceso = models.ForeignKey(Proceso, on_delete=models.CASCADE)
+    delaComparecencia = models.ForeignKey(Comparecencia, on_delete=models.CASCADE)
+    delaAutoridad = models.ForeignKey(AutoridadActuante, on_delete=models.CASCADE)
+    contstanciaAdmision_Rechazo = models.FileField(verbose_name="Constancia de Admisión/Rechazo:",upload_to='files/', null=True, blank=True)
+    acuerdoSuspension = models.FileField(verbose_name="Acuerdo Suspensión:",upload_to='files/', null=True, blank=True)
+
+CONDCION = (
+    ('Victima', 'VICTIMA'),
+    ('Testigo', 'TESTIGO'),
+)
+
+class NotificacionFiscalia(models.Model):
+    delaEstacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
+    fechaNotificacion= models.DateField()
+    horaNotificacion = models.DateTimeField()
+    numeroOficio = models.CharField(max_length=50)
+    delaFiscalia = models.ForeignKey(Fiscalia, on_delete=models.CASCADE)
+    delaComparecencia = models.ForeignKey(Comparecencia, on_delete=models.CASCADE)
+    condicion = models.CharField(max_length=50, choices= CONDCION)
+    documentoFGR = models.CharField(max_length=100)
+
+
